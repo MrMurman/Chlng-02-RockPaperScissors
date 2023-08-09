a19c8c545878d8ebc413f967aa76ab8c6f36ee13
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-09 19:21:24 +0300

    Initial Commit

---
+++ Chlng-02-RockPaperScissors/Chlng_02_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chlng_02_RockPaperScissorsApp.swift
+//  Chlng-02-RockPaperScissors
+//
+//  Created by Андрей Бородкин on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Chlng_02_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chlng-02-RockPaperScissors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Chlng-02-RockPaperScissors
+//
+//  Created by Андрей Бородкин on 09.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
