5480c231e5f67b21a98c22d0a577bd1a54f12495
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-12 12:55:49 +0300

    no mmsg

--- Chlng-02-RockPaperScissors/ContentView.swift
+++ Chlng-02-RockPaperScissors/ContentView.swift
@@ -8,14 +8,113 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    // MARK: - Initial setup
+    
+    enum Sign: String {
+        case rock = "👊"
+        case scissors = "✌️"
+        case paper = "🖐️"
+    }
+    
+    @State var signsArray: [Sign] = [.rock, .scissors, .paper]
+    
+    @State var shouldWin = Bool.random()
+    @State var computerMove = Int.random(in: 0...2)
+    
+    var computerSign: Sign {
+        let sign = signsArray[computerMove]
+        return sign
+    }
+    
+    @State var playerSign: Sign = .paper
+    
+    var prompt: String {
+        shouldWin ? "You need to win" : "You need to loose"
+    }
+    
+    // MARK: - Game logics
+    
+    @State var round = 0
+    @State var score = 0
+    
+    
+    func compareSigns(signA: Sign, signB: Sign) -> Int {
+        switch signA {
+        case .paper:
+            switch signB {
+            case .paper: return 0
+            case .rock: return 1
+            case .scissors: return -1
+            }
+        case .rock:
+            switch signB {
+            case .paper: return -1
+            case .rock: return 0
+            case .scissors: return 1
+            }
+        case .scissors:
+            switch signB {
+            case .paper: return 1
+            case .rock: return -1
+            case .scissors: return 0
+            }
         }
-        .padding()
+    }
+    
+    
+    func nextRound() {
+        round += 1
+        if round == 10 {
+            round = 0
+            score = 0
+        }
+        
+        computerMove = Int.random(in: 0...2)
+        shouldWin.toggle()
+        
+    }
+    
+    func signButtonTapped(sign: Sign) {
+        playerSign = sign
+        
+        if shouldWin {
+            score += compareSigns(signA: playerSign, signB: computerSign)
+        } else {
+            score -= compareSigns(signA: playerSign, signB: computerSign)
+        }
+        
+       nextRound()
+        
+    }
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Pick your sign")
+                .font(.largeTitle)
+            Text(prompt)
+                .font(.headline)
+            VStack {
+                ForEach(signsArray, id: \.self) {item in
+                    
+                    Button {
+                        signButtonTapped(sign: item)
+                        
+                    } label: {
+                        Text(item.rawValue)
+                            .font(.system(size: 72))
+                            .frame(width: 100, height: 100)
+                            .background( item == computerSign ? Color.red : Color.white)
+                    }
+                }
+            }
+            .padding()
+            
+            Text("Your score is \(score)")
+                .font(.headline)
+            Text("Round \(round)")
+                .font(.subheadline)
+        }
     }
 }
 
